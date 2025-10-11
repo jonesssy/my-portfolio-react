9a1915e0637541d28aae3efbe9fc1a8cd4617fd7
Joni Mitronen <joni.mitronen@gmail.com>
2025-10-11 16:44:07 +0300

    removed some comments

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,3 @@
-// // components/Navbar.js
-// import React from "react";
-
-
-// export default function Navbar() {
-//   const items = ["home", "life", "skills", "passions", "projects"];
-
-//   const scrollTo = (id) => {
-//     const el = document.getElementById(id);
-//     if (el) el.scrollIntoView({ behavior: "smooth" });
-//   };
-
-//   return (
-//     <header>
-//       <nav>
-//         {items.map((id) => (
-//           <button key={id} onClick={() => scrollTo(id)}>
-//             {id}
-//           </button>
-//         ))}
-//       </nav>
-//     </header>
-//   );
-// }
-
-
 import React, { useState } from "react";
 
 export default function Navbar() {
