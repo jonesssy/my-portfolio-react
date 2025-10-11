784b1b0505ababf7fd78cadbcb2eb40bebaa248a
Joni Mitronen <joni.mitronen@gmail.com>
2025-10-11 17:05:05 +0300

    size chances

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Projects from "../components/Projects";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  const sectionStyle = { maxWidth: 1500, margin: "0 auto", padding: "0" }; // padding-top includes navbar + breathing room
+  const sectionStyle = { maxWidth: 1200, margin: "0 auto", padding: "0" }; // padding-top includes navbar + breathing room
 
   return (
     <>
