5927bdc785fbc56c4202577b302c5cc2e4958659
Joni Mitronen <joni.mitronen@gmail.com>
2025-10-14 18:39:05 +0300

    hero text before pic in mobile

--- components/Hero.js
+++ components/Hero.js
@@ -8,15 +8,15 @@ export default function Hero() {
 
     return (
         <div className="hero">
-            <div className="hero-image">
-                <img src="/media/images/chilling.jpg" alt="profile" id="profileImage" />
-            </div>
-
-            {/* Right side - Text */}
             <div className="hero-text">
                 <h1>{hero.welcome}</h1>
                 <p>{hero.p1}<span className="highlight-name">{hero.name}</span>{hero.p2}</p>
             </div>
+            <div className="hero-image">
+                <img src="/media/images/chilling.jpg" alt="profile" id="profileImage" />
+            </div>
+
+
         </div>
     );
 }
