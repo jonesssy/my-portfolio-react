45e27e22040b8dcd86ee39d95124d76f20a3694e
Joni Mitronen <joni.mitronen@gmail.com>
2025-10-15 12:14:23 +0300

    shoter paragraps for hobbies

--- components/Hobbies.js
+++ components/Hobbies.js
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { useLanguage } from "../context/LanguageContext";
-
-// export default function Hobbies() {
-//     const { content } = useLanguage();
-//     const hobbiesContent = content.hobbies || {};
-//     const hobbies = hobbiesContent.hobbies || [];
-
-//     return (
-//         <section className="hobbies-section">
-//             <h2 className="hobbies-title">{hobbiesContent.title || "Hobbies"}</h2>
-
-//             <div className="hobbie-grid">
-//                 {hobbies.map((p, index) => (
-//                     <div key={index} className="hobbie-card">
-//                         <h3>{p.hobbie}</h3>
-//                         <img src={p.imgSrc} alt={p.alt || p.hobbie} />
-//                     </div>
-//                 ))}
-//             </div>
-//         </section>
-//     );
-// }
-
 import React, { useState } from "react";
 import { useLanguage } from "../context/LanguageContext";
 
